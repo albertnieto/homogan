8b6e2834ebe34ac046e79d1e3b967891185d2c11
Mauro <mauroalvarezestarlich@gmail.com>
2020-03-16 22:23:55 +0100

    Remove conv2D of generator
    Remove a layer from discrim

--- src/Experiment3/GAN.py
+++ src/Experiment3/GAN.py
@@ -15,12 +15,12 @@ class Generator(tf.keras.Model):
         self.reshape = layers.Reshape((8,8,128))
 
         self.unconv1 = layers.Conv2DTranspose(128, kernel_size=(5,5) ,  strides=(2,2), padding="same", use_bias=False)
-        self.conv1 = layers.Conv2D( 64  , ( 1 , 1 ) , padding='same', name="block_4", use_bias=False)
+        #self.conv1 = layers.Conv2D( 64  , ( 1 , 1 ) , padding='same', name="block_4", use_bias=False)
         self.batchNorm3 = layers.BatchNormalization()
         self.leakyRelu3 = layers.LeakyReLU()
 
         self.unconv2 = layers.Conv2DTranspose(32, kernel_size=(5,5) ,  strides=(2,2), padding="same", use_bias=False)
-        self.conv2 = layers.Conv2D( 64  , ( 1 , 1 ) , padding='same', name="block_5", use_bias=False)
+        #self.conv2 = layers.Conv2D( 64  , ( 1 , 1 ) , padding='same', name="block_5", use_bias=False)
         self.batchNorm4 = layers.BatchNormalization()
         self.leakyRelu4 = layers.LeakyReLU()
                 
@@ -42,10 +42,10 @@ class Generator(tf.keras.Model):
         x = self.reshape(x)
 
         x = self.unconv1(x)
-        x = self.leakyRelu3(self.batchNorm3(self.conv1(x)))
+        x = self.leakyRelu3(self.batchNorm3(x))
 
         x = self.unconv2(x)
-        x = self.leakyRelu4(self.batchNorm4(self.conv2(x)))
+        x = self.leakyRelu4(self.batchNorm4(x))
 
         x = self.unconv3(x)
         x = self.leakyRelu5(self.batchNorm5(self.conv3(x)))
@@ -67,29 +67,29 @@ class Discriminator(tf.keras.Model):
         self.conv1 = layers.Conv2D(32, (5, 5), padding='same', input_shape=[128, 128, 3], name='block1_conv1')
         self.batchNorm1 = layers.BatchNormalization()
         self.leakyRelu1 = layers.LeakyReLU()
-        self.dropout1 = layers.Dropout(0.3)
+        self.dropout1 = layers.Dropout(0.5)
 
-        self.conv2 = layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', name='block1_conv2')
-        self.batchNorm2 = layers.BatchNormalization()
-        self.leakyRelu2 = layers.LeakyReLU()
-        self.dropout2 = layers.Dropout(0.3)
+        # self.conv2 = layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', name='block1_conv2')
+        # self.batchNorm2 = layers.BatchNormalization()
+        # self.leakyRelu2 = layers.LeakyReLU()
+        # self.dropout2 = layers.Dropout(0.5)
 
         self.conv3 = layers.Conv2D(128, (3, 3), strides=(2, 2), padding='same', name='block1_conv3')
         self.batchNorm3 = layers.BatchNormalization()
         self.leakyRelu3 = layers.LeakyReLU()
-        self.dropout3 = layers.Dropout(0.3)
+        self.dropout3 = layers.Dropout(0.5)
 
         self.conv4 = layers.Conv2D(256, (3, 3), strides=(2, 2), padding='same', name='block1_conv4')
         self.batchNorm4 = layers.BatchNormalization()
         self.leakyRelu4 = layers.LeakyReLU()
-        self.dropout4 = layers.Dropout(0.3)
+        self.dropout4 = layers.Dropout(0.5)
 
         self.flatten = layers.Flatten()
         self.dense1 = layers.Dense(output_shape)
     
     def call(self, inputs):
         x = self.dropout1(self.leakyRelu1(self.batchNorm1(self.conv1(inputs))))
-        x = self.dropout2(self.leakyRelu2(self.batchNorm2(self.conv2(x))))
+        # x = self.dropout2(self.leakyRelu2(self.batchNorm2(self.conv2(x))))
         x = self.dropout3(self.leakyRelu3(self.batchNorm3(self.conv3(x))))
         x = self.dropout4(self.leakyRelu4(self.batchNorm4(self.conv4(x))))
 
