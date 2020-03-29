72e323cf7c76afe7c0728488cf8794217b457292
Jordi Sans <jordisanspenadev@gmail.com>
2020-03-29 13:34:06 +0200

    fix exp8 and add 9 and 10

--- src/Experiment10/main.py
+++ src/Experiment10/main.py
@@ -34,20 +34,30 @@ tf.keras.backend.set_floatx('float32')
 
 ####Prepare dataset
 
-celeba = CelebA()
+features = ['Male']
+celeba = CelebA(selected_features=features, main_folder='/content/celeba-dataset')
 
-data_dir = pathlib.Path(celeba.images_folder + "")
-image_count = len(list(data_dir.glob('*/*.jpg')))
-image_list = list(data_dir.glob('*/*.jpg'))
-image_list = [str(x) for x in image_list]
-len(image_list)
-print(len(list(data_dir.glob('*/*.jpg'))))
+feat_df = celeba.attributes
+feat_male = feat_df[feat_df.Male == 1][:5000]
+feat_female = feat_df[feat_df.Male == 0][:5000]
+
+feat_male['image_id'] = feat_male['image_id'].apply(
+  lambda x: '/content/celeba-dataset/img_align_celeba/img_align_celeba/'+x)
+feat_female['image_id'] = feat_female['image_id'].apply(
+  lambda x: '/content/celeba-dataset/img_align_celeba/img_align_celeba/'+x)
+
+image_list = feat_male['image_id'].tolist()
+image_list = image_list + feat_female['image_id'].tolist()
+
+print(len(image_list))
+# print(image_list)
+# exit()
 
 IMG_HEIGHT = 128
 IMG_WIDTH = 128
 BUFFER_SIZE = 3000
 BATCH_SIZE = 100
-NUM_IMAGES_USED = 10000
+NUM_IMAGES_USED = len(image_list)
 noise_dim = 256
 STEPS_PER_EPOCH = np.ceil(NUM_IMAGES_USED/BATCH_SIZE)
 CLASS_NAMES = celeba.features_name
@@ -120,69 +130,10 @@ checkpoint = tf.train.Checkpoint(generator=generator,
 manager = tf.train.CheckpointManager(checkpoint, directory = checkpoint_dir, max_to_keep=3)
 
 EPOCHS = 100
-num_examples_to_generate = 4
+train_GEN = 3 #Train every 3 batches
+train_DISC = 1 #Train every batch
 
-# def train(dataset, epochs):
-#     checkpoint.restore(manager.latest_checkpoint)
-#     if manager.latest_checkpoint:
-#         print("Restored from {}".format(manager.latest_checkpoint))
-#     else:
-#         print("Initializing from scratch.")
-
-#     logdir = '.\\logs\\func\\'
-#     if not os.path.exists(logdir):
-#         os.makedirs(logdir)
-
-#     writer = tf.summary.create_file_writer(logdir)
-
-#     tf.summary.trace_on(graph=True, profiler=True)
-
-#     cycle = 0
-#     start_train = time.time()
-#     for epoch in range(epochs):
-#         start = time.time()
-#         i = 0
-#         for image_batch in dataset:
-#             genL, discL, gen_grad, disc_grad, real_acc, fake_acc = train_step(image_batch, epoch)
-
-#             i += 1
-#             cycle += 1
-#             if i % 100 == 0:
-#                 print(f"Batch {i}/{STEPS_PER_EPOCH}")
-
-#                 with writer.as_default():
-#                     tf.summary.scalar('real acc', real_acc, step=cycle)
-#                     tf.summary.scalar('fake acc', fake_acc, step=cycle)
-#                     tf.summary.scalar('Gen Loss', genL, step=cycle)
-#                     tf.summary.scalar('Disc Loss', discL, step=cycle)
-#         if (time.time() - start_train) > 10*60: #Log cada 10 min
-#             start_train = time.time()
-#             with writer.as_default():
-#                 for grad in gen_grad:
-#                     tf.summary.histogram('Gen_grad', grad, step=cycle)
-#                 for grad in disc_grad:
-#                     tf.summary.histogram('Disc_grad', grad, step=cycle)
-
-#         generate_and_save_images(generator,
-#                                  epoch + 1,
-#                                  tf.random.normal([num_examples_to_generate,1,1, noise_dim]),
-#                                  titleadd="Epoch {}".format(epoch))
-
-#         # Save the model every 20 epochs
-#         if (epoch + 1) % 20 == 0 or epoch == epochs-1:
-#             checkpoint.save(file_prefix = checkpoint_prefix)
-
-
-#         print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-
-#     # Generate after the final epoch
-#     generate_and_save_images(generator,
-#                            epochs,
-#                            tf.random.normal([num_examples_to_generate,1,1, noise_dim]))
-
-# print(manager.latest_checkpoint)
-
-def train(g_model, d_model, gan_model, dataset, latent_dim=100, n_epochs=100, n_batch=128):
+def train(g_model, d_model, gan_model, dataset, latent_dim=100, n_epochs=100, train_GEN = 1, train_DISC = 1):
     checkpoint.restore(manager.latest_checkpoint)
     if manager.latest_checkpoint:
         print("Restored from {}".format(manager.latest_checkpoint))
@@ -204,25 +155,32 @@ def train(g_model, d_model, gan_model, dataset, latent_dim=100, n_epochs=100, n_
         x = 0
         # enumerate batches over the training set
         for image_batch in dataset:
-            if cycle % 10 == 0:
+            n_batch = image_batch[0].shape[0]
+            if cycle % 50 == 0:
               print(f"Batch: {x}/{NUM_IMAGES_USED/n_batch}")
             x += 1
             cycle += 1
             # get randomly selected 'real' samples
-            X_real = image_batch
+            X_real = image_batch[0]
             y_real = tf.ones(n_batch,1)
-            # update discriminator model weights
-            d_loss1, _ = d_model.train_on_batch(X_real, y_real)
-            # generate 'fake' examples
-            X_fake, y_fake = gan.generate_fake_samples(g_model, latent_dim, n_batch)
-            # update discriminator model weights
-            d_loss2, _ = d_model.train_on_batch(X_fake, y_fake)
-            # prepare points in latent space as input for the generator
-            X_gan = gan.generate_latent_points(latent_dim, n_batch)
-            # create inverted labels for the fake samples
-            y_gan = np.ones((n_batch, 1))
-            # update the generator via the discriminator's error
-            g_loss = gan_model.train_on_batch(X_gan, y_gan)
+            # smoothing
+            y_real = smooth_pos_and_trick(y_real)
+            if cycle % train_DISC == 0:
+              # update discriminator model weights
+              d_loss1, _ = d_model.train_on_batch(X_real, y_real)
+              # generate 'fake' examples
+              X_fake, y_fake = gan.generate_fake_samples(g_model, latent_dim, n_batch)
+              # smoothing
+              y_fake = smooth_neg_and_trick(y_fake)
+              # update discriminator model weights
+              d_loss2, _ = d_model.train_on_batch(X_fake, y_fake)
+            if cycle % train_GEN == 0:
+              # prepare points in latent space as input for the generator
+              X_gan = gan.generate_latent_points(latent_dim, n_batch)
+              # create inverted labels for the fake samples
+              y_gan = np.ones((n_batch, 1))
+              # update the generator via the discriminator's error
+              g_loss = gan_model.train_on_batch(X_gan, y_gan)
         
             if cycle % 10 == 0:
               with writer.as_default():
@@ -232,7 +190,7 @@ def train(g_model, d_model, gan_model, dataset, latent_dim=100, n_epochs=100, n_
         # summarize loss on this batch
         print('Epoch: %d,  Loss: D_real = %.3f, D_fake = %.3f,  G = %.3f' %   (i+1, d_loss1, d_loss2, g_loss))
         # evaluate the model performance
-        if (i+1) % 1 == 0:
+        if (i+1) % 5 == 0:
             gan.summarize_performance(i, g_model, d_model, image_batch, latent_dim)     
         if (i+1) % 20 == 0:
           # Save the model every 10 epochs
@@ -243,6 +201,6 @@ def train(g_model, d_model, gan_model, dataset, latent_dim=100, n_epochs=100, n_
 theGan = gan.define_gan(generator, discriminator)
 
 with tf.device('/device:GPU:0'):
-    train(generator, discriminator, theGan, training_dataset, 256, 100,100)
+    train(generator, discriminator, theGan, training_dataset, noise_dim, EPOCHS, train_GEN, train_DISC)
 
 # checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
