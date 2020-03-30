d4090bbb6883b760b0a010563dd26645881a9b28
jordisans <jordisanspenadev@gmail.com>
2020-03-30 18:41:55 +0000

    Update experiment 12

--- src/Experiment12/data_importer.py
+++ src/Experiment12/data_importer.py
@@ -7,7 +7,7 @@ class CelebA():
              - Select the features of interest,
              - Split the dataset into 'training', 'test' or 'validation' partition.
     '''
-    def __init__(self, main_folder='/content/celeba-dataset', selected_features=None, drop_features=[]):
+    def __init__(self, main_folder, selected_features=None, drop_features=[]):
         self.main_folder = main_folder
         self.images_folder   = os.path.join(main_folder, 'img_align_celeba/')
         self.attributes_path = os.path.join(main_folder, 'list_attr_celeba.csv')
